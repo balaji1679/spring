70544a433e5b6847679419ac431e27fd41762135
balaji1679 <108862550+balaji1679@users.noreply.github.com>
2023-05-11 20:21:45 +0530

    Add files via upload

---
+++ SprBootRestApi-2/src/main/java/com/alibou/SprBootRestApi2Application.java
@@ -0,0 +1,13 @@
+package com.alibou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprBootRestApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprBootRestApi2Application.class, args);
+	}
+
+}
---
+++ SprBootRestApi-2/src/test/java/com/alibou/SprBootRestApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprBootRestApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
